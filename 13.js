d16578948d8a083892e1910c4fc53ae0f07ee6cb
light <raoyangkm@gmail.com>
2023-03-15 16:02:09 +0800

    13. Arrow Functions

---
+++ 13.js
@@ -0,0 +1,12 @@
+function printMyName(name) {
+  console.log(name);
+}
+printMyName("Max");
+
+const printMyName2 = (name, age) => {
+  console.log(name, age);
+};
+printMyName2("Maru", 22);
+
+const multiply = (number) => number * 2;
+console.log(multiply(3));
