7118c9e8bb35cbe20276b0d6a9a75649b9f3945b
light <raoyangkm@gmail.com>
2023-03-17 17:03:09 +0800

    18. Destructuring

---
+++ 18.js
@@ -0,0 +1,13 @@
+[a, b] = ["hello", "world"];
+console.log(a);
+console.log(b);
+
+[num1, , num3] = [1, 2, 3, 4];
+console.log(num1);
+console.log(num3);
+
+// let person= {name:'ZhangSan', age:22, weight:70}
+// {name,age} = person;
+// console.log(name);
+// console.log(age);
+// console.log(weight);
