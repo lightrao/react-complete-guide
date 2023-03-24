50397b4a76945b686ff46f77a674b2f47984ebeb
light <raoyangkm@gmail.com>
2023-03-24 15:44:02 +0800

    29. Analyzing a Standard React Project

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,7 +1,9 @@
-import ReactDOM from 'react-dom/client';
+// the first file to be executed
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App"; // JSX component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// chose the div tag in index.html with id of "root" as a start point at which to render react UI
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
