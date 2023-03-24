23db1aa4e4bd7fb51a425b6547004a5e228f0ba7
light <raoyangkm@gmail.com>
2023-03-24 16:30:35 +0800

    31. How React Works

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,8 +1,10 @@
 function App() {
   return (
     // Under hood JSX will be transformed to javascript code which browser can run
+    // declarative way different from imperative way(just javascript)
     <div>
       <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
     </div>
   );
 }
