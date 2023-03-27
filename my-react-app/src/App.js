36fda60d7b608d26296717d02aae12ce892f27bc
light <raoyangkm@gmail.com>
2023-03-27 10:46:21 +0800

    32. Building a First Custom Component

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     // Under hood JSX will be transformed to javascript code which browser can run
     // declarative way different from imperative way(just javascript)
     <div>
       <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ my-react-app/src/components/ExpenseItem.js
@@ -0,0 +1,5 @@
+function ExpenseItem() {
+  return <h2>Expense itme!</h2>;
+}
+
+export default ExpenseItem;
