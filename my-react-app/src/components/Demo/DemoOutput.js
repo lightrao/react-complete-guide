57501a1682150b506e3d22d4ee830436d1ab225e
light <raoyangkm@gmail.com>
2023-06-20 16:43:46 +0800

    A Closer Look At Child Component Re-Evaluation

---
+++ my-react-app/src/components/Demo/DemoOutput.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import MyParagraph from "./MyParagraph";
+
+const DemoOutput = (props) => {
+  console.log("DemoOutput running...");
+
+  return <MyParagraph>{props.show ? "This is new!" : ""}</MyParagraph>;
+};
+
+export default DemoOutput;
