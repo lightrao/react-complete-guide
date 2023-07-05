d17f566af06b2856932f02c01fb4b9ab8323bc34
light <raoyangkm@gmail.com>
2023-07-05 10:44:28 +0800

    The Component Lifecycle

---
+++ my-react-app/src/components/UserFinder.js
@@ -0,0 +1,43 @@
+import { Fragment, useState, useEffect } from "react";
+
+import Users from "./Users";
+import classes from "./UserFinder.module.css";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+  { id: "u4", name: "John" },
+  { id: "u5", name: "Jane" },
+  { id: "u6", name: "David" },
+  { id: "u7", name: "Samantha" },
+  { id: "u8", name: "Liam" },
+  { id: "u9", name: "Emma" },
+];
+
+const UserFinder = () => {
+  const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    setFilteredUsers(
+      DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+    );
+  }, [searchTerm]);
+
+  const searchChangeHandler = (event) => {
+    console.log("searchChangeHandler are running...");
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <Fragment>
+      <div className={classes.finder}>
+        <input type="search" onChange={searchChangeHandler} />
+      </div>
+      <Users users={filteredUsers} />
+    </Fragment>
+  );
+};
+
+export default UserFinder;
