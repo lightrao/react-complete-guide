0697031fea38bf5f6715eb48654489822790a5fd
light <raoyangkm@gmail.com>
2023-09-07 15:54:58 +0800

    Dispatching Actions From Inside Components

--- my-react-app/src/components/Counter.js
+++ my-react-app/src/components/Counter.js
@@ -1,12 +1,21 @@
-// allows us to select a part of state managed by the store
-import { useSelector } from "react-redux";
+// "useSelector" allows us to select a part of state managed by the store
+import { useSelector, useDispatch } from "react-redux";
 
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const dispatch = useDispatch();
+
   // param function will be executed by redux to get part of state
   // redux will automatically manage a subscription to the redux store for the current component("Counter")
   const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -14,6 +23,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
