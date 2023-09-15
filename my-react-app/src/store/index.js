a328fc1ae4eef60db546b2aea419ab16ee8bd38c
light <raoyangkm@gmail.com>
2023-09-15 17:11:40 +0800

    Refresher / Practice: Part 2/2

--- my-react-app/src/store/index.js
+++ my-react-app/src/store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import uiSliceReducer from "./ui-slice";
+import cartSliceReducer from "./cart-slice";
 
 const myReduxStore = configureStore({
-  reducer: { uiSliceReducer },
+  reducer: { uiSliceReducer, cartSliceReducer },
 });
 
 export default myReduxStore;
