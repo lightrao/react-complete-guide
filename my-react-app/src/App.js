c741df80facc221bfe38eecab5e366c3ce7159ed
light <raoyangkm@gmail.com>
2023-09-19 15:53:05 +0800

    Using useEffect with Redux

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
@@ -6,6 +7,20 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.uiSliceReducer.cartIsVisible);
+
+  // useSelector sets up a subscription to Redux, whenever Redux store does change,
+  // the "App" component function will be re-executed and we will get to the latest state of "cart"
+  const cart = useSelector((state) => state.cartSliceReducer);
+
+  useEffect(() => {
+    fetch(
+      "https://react-http-post-example-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+      {
+        method: "PUT", // override existing data
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
 
   return (
     <Layout>
