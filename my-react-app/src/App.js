2cfa441671f0276c6566a0dc0d8f78e64a7dc2e8
light <raoyangkm@gmail.com>
2023-09-20 11:05:01 +0800

    Handling Http States & Feedback with Redux

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,12 +1,21 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, Fragment } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+
+import { uiActions } from "./store/ui-slice";
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.uiSliceReducer.cartIsVisible);
+  const notification = useSelector(
+    (state) => state.uiSliceReducer.notification
+  );
 
   // useSelector sets up a subscription to Redux, whenever Redux store does change,
   // the "App" component function will be re-executed and we will get to the latest state of "cart"
@@ -15,20 +24,66 @@ function App() {
   // execute when our app starts, It's a problem because this will send the initial (i.e. empty)
   // cart to our backend and overwrite any data stored there.
   useEffect(() => {
-    fetch(
-      "https://react-http-post-example-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
-      {
-        method: "PUT", // override existing data
-        body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://react-http-post-example-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT", // override existing data
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed!");
       }
-    );
-  }, [cart]);
+      // const responseData = await response.json();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
