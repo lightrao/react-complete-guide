e8d7867c4fa9fa6cc7d1cdaa53a8f671df170081
light <raoyangkm@gmail.com>
2023-09-25 11:02:05 +0800

    Layouts & Nested Routes

---
+++ my-react-app/src/pages/Root.js
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+
+import MainNavigation from "../components/MainNavigation";
+import classes from "./Root.module.css";
+
+const RootLayout = (props) => {
+  return (
+    <>
+      <MainNavigation />
+      <main className={classes.content}>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default RootLayout;
