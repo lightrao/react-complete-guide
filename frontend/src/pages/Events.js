9f7ccaa143c7a1e05e7558bb6ac4c98d0fd01881
light <raoyangkm@gmail.com>
2023-10-13 10:33:01 +0800

    Returning Responses in loader()s

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -3,8 +3,8 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  // get access to the "closest loader data"
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events;
 
   return (
     <>
@@ -21,16 +21,6 @@ export const loader = async () => {
   if (!response.ok) {
     // do sth
   } else {
-    const resData = await response.json();
-    // my comment:
-    // you can access loaded data with help of useLoaderData() in any component on the same level
-    // or lower level. that means: you can use useLoaderData() in the element(component) that assgined to
-    // the route And in all components(subcomponents) that might be used inside that component
-
-    // gpt comment:
-    // The loader function fetches the events data from the server and returns it.
-    // You can use the useLoaderData hook to get the data in any component that is
-    // the element of this route or its subcomponents.
-    return resData.events;
+    return response;
   }
 };
