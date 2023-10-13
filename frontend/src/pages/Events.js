aa02e46c962164cb9ded16ef2194adc2a5f2c593
light <raoyangkm@gmail.com>
2023-10-13 10:52:29 +0800

    first way of dealing error

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -6,6 +6,10 @@ function EventsPage() {
   const data = useLoaderData();
   const events = data.events;
 
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
+
   return (
     <>
       <EventsList events={events} />
@@ -22,7 +26,7 @@ export const loader = async () => {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // do sth
+    return { isError: true, message: "Could not fetch events." };
   } else {
     return response;
   }
