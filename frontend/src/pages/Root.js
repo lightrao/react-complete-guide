37bbc4b6b544a7c02f57cd3fb808bdaff8c15d59
light <raoyangkm@gmail.com>
2023-10-13 10:17:58 +0800

    Reflecting The Current Navigation State in the UI

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,11 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet /* useNavigation */ } from "react-router-dom";
 import MainNavigaition from "../components/MainNavigation";
 
 function RootLayout(props) {
+  // const navigation = useNavigation();
+
   return (
     <>
       <MainNavigaition />
       <main>
+        {/* {navigation.state === "loading" && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
